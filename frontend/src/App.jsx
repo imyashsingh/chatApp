1074dc9fa705c6d2a1d41c1838462323f41756e7
imyashsingh <yash.singh5445@gmail.com>
2024-02-12 18:25:08 +0530

    added ligin Page Ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Login from "./pages/login/Login";
 
 function App() {
     return (
-        <>
-            <div>react lakdmak</div>
-        </>
+        <div className="p-4 h-screen flex items-center justify-center">
+            <Login />
+        </div>
     );
 }
 
